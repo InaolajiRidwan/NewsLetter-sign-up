10779446650410ee72fa7775038fe494f53655f0
Inaolaji Ridwan <ridwaninaolaji@gmail.com>
2024-02-05 11:22:33 +0100

    first commit, i an the issue of email validation which i later reolved in main.js line 8

---
+++ main.js
@@ -0,0 +1,21 @@
+let input = document.getElementById("email");
+let errorMessage = document.getElementById("error");
+let successMessage = document.getElementById("success");
+let cardContent = document.getElementById("card");
+let imageConfusion = document.getElementById("imgconfusion");
+
+function loginbtn() {
+  if (!input.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+    errorMessage.style.display = "block";
+  } else {
+    successMessage.style.display = "block";
+    cardContent.style.display = "none";
+    document.getElementById("show").innerHTML = input.value;
+  }
+}
+
+function successbtn() {
+  cardContent.style.display = "";
+  successMessage.style.display = "none";
+  input.value = "";
+}
